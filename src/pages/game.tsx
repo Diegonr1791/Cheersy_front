ef6b484a4145b0d6ba98fe71874273a08a326331
Hernan dP <hernandp@nimble.la>
2023-09-15 14:28:21 -0300

    Fixed global style

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,7 +1,40 @@
-const Game = () => {
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+  // This reference will give us direct access to the THREE.Mesh object
+  const ref = useRef<THREE.Mesh>(null!);
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+
   return (
-    <div>Game</div>
-  )
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
 }
 
-export default Game
+const Game = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+};
+
+export default Game;
