12650e3c4fa6078faa100d52982da3d162038e75
Hernan dP <hernandp@nimble.la>
2023-09-15 13:19:14 -0300

    Added React query and three js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import NavigationProvider from "./providers/navigation";
-import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import './App.css'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <NavigationProvider />
-    </>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <NavigationProvider />
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Link } from "react-router-dom";
+import { HStack, VStack } from "@chakra-ui/react";
 
 function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <VStack w="full" h="100vh" justifyContent="center" alignItems="center" gap={5}>
+      <HStack>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </HStack>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,10 +29,8 @@ function Home() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Link to={`/game`}>
-        Go To Game
-      </Link>
-    </>
+      <Link to={`/game`}>Go To Game</Link>
+    </VStack>
   );
 }
 
